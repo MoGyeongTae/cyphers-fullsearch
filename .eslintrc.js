bc40990776a29be741da4743d8570dbc6240843e
MoGyeongTae <alska2586@naver.com>
2022-12-02 13:03:17 +0900

    chore: update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,53 +1,52 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   extends: [
-    "airbnb",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "react-app",
-    "plugin:react-perf/recommended"
+    'plugin:react-perf/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommend',
   ],
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
-    include: ["src/**/*.tsx", "src/**/*.ts"],
-    exclude: ["public/*", "src/**/*.d.tsx"],
+    include: ['src/**/*.tsx', 'src/**/*.ts'],
+    exclude: ['public/*', 'src/**/*.d.tsx'],
     ecmaFeatures: {
-      jsx: true,  
-    }
+      jsx: true,
+    },
   },
   settings: {
-    "import/extensions": [".js", ".ts", ".tsx"],
-    "import/parsers": {
-      "@typescript-eslint/parser": [".js", ".ts", ".tsx"]
+    'import/extensions': ['.js', '.ts', '.tsx'],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.js', '.ts', '.tsx'],
     },
-    "import/resolver": {
+    'import/resolver': {
       node: {
-        extensions: [".js", ".ts", ".tsx"]
-      }
-    }
+        extensions: ['.js', '.ts', '.tsx'],
+      },
+    },
   },
   rules: {
-    "import/extensions": [
-      "error",
-      "ignorePackages",
+    'import/extensions': [
+      'error',
+      'ignorePackages',
       {
-        "js": "never",
-        "jsx": "never",
-        "ts": "never",
-        "tsx": "never"
-      }
-   ],
-    "react/jsx-filename-extension": [
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'react/jsx-filename-extension': [
       1,
       {
-        extensions: [".tsx"]
-      }
+        extensions: ['.tsx'],
+      },
     ],
-    '@typescript-eslint/camelcase': ['error', { 'properties': 'never' }],
+    '@typescript-eslint/camelcase': ['error', { properties: 'never' }],
     '@typescript-eslint/no-var-requires': ['warn'],
-    'key-spacing': ['error', { 'align': 'value' }],
+    'key-spacing': ['error', { align: 'value' }],
     'react/prop-types': ['off'],
-    "@typescript-eslint/indent": ["error", 2],
-    "max-classes-per-file":['off'],
+    '@typescript-eslint/indent': ['error', 2],
+    'max-classes-per-file': ['off'],
   },
 };
